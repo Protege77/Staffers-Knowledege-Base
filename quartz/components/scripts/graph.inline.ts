8edf4c61c47a9305e0c75a53629c30f0c5e5bb24
Herman <herman1977@gmail.com>
2026-05-27 15:42:45 +0800

    Increase graph node size for easier clicking

--- quartz/components/scripts/graph.inline.ts
+++ quartz/components/scripts/graph.inline.ts
@@ -209,7 +209,7 @@ async function renderGraph(graph: HTMLElement, fullSlug: FullSlug) {
     const numLinks = graphData.links.filter(
       (l) => l.source.id === d.id || l.target.id === d.id,
     ).length
-    return 2 + Math.sqrt(numLinks)
+    return 6 + Math.sqrt(numLinks) * 2
   }
 
   let hoveredNodeId: string | null = null
